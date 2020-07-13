ae6e4f6ed1add29cced941df0f51e189eb16a863
MuhammadNauman <56251248+justnaumanbhatti@users.noreply.github.com>
2020-07-14 00:35:05 +0500

    implementation of our algorithum

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Algorithm_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the string:");
+            string input = Console.ReadLine();
+            HuffmanTree huffmanTree = new HuffmanTree();
+
+            // Build the Huffman tree
+            huffmanTree.Build(input);
+
+            // Encode
+            BitArray encoded = huffmanTree.Encode(input);
+
+            Console.Write("Encoded: ");
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+
+            // Decode
+            string decoded = huffmanTree.Decode(encoded);
+
+            Console.WriteLine("Decoded: " + decoded);
+
+            Console.ReadLine();
+        }
+    }
+}
