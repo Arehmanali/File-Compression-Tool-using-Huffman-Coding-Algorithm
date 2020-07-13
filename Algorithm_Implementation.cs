0c8cdb246193dcd7f5f7c42b7bbdec9bc6f9c6db
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2020-07-13 11:17:32 -0700

    implemented the algorithm

---
+++ Algorithm_Implementation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
