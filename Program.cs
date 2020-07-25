1a5a7c2caec2b267d1573e46763bf8ea1414be40
MuhammadNauman <56251248+justnaumanbhatti@users.noreply.github.com>
2020-07-26 00:28:57 +0500

    front end of project

--- Program.cs
+++ Program.cs
@@ -1,38 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Collections;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Algorithm_Implementation
+namespace WindowsFormsApp6
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Please enter the string:");
-            string input = Console.ReadLine();
-            HuffmanTree huffmanTree = new HuffmanTree();
-
-            // Build the Huffman tree
-            huffmanTree.Build(input);
-
-            // Encode
-            BitArray encoded = huffmanTree.Encode(input);
-
-            Console.Write("Encoded: ");
-            foreach (bool bit in encoded)
-            {
-                Console.Write((bit ? 1 : 0) + "");
-            }
-            Console.WriteLine();
-
-            // Decode
-            string decoded = huffmanTree.Decode(encoded);
-
-            Console.WriteLine("Decoded: " + decoded);
-
-            Console.ReadLine();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
