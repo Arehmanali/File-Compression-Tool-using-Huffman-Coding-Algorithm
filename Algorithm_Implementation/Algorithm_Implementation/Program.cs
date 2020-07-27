30baf14b17d92ebe061ad0a4c01fe1b90dc5b397
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2020-07-28 01:29:01 +0500

    Update Program.cs

--- Algorithm_Implementation/Algorithm_Implementation/Program.cs
+++ Algorithm_Implementation/Algorithm_Implementation/Program.cs
@@ -8,6 +8,8 @@
     using iTextSharp.text;
     using iTextSharp.text.pdf;
     using iTextSharp.text.pdf.parser;
+    using GemBox.Document;
+using System.Drawing;
 
 namespace Algorithm_Implementation
     {
@@ -24,14 +26,10 @@ namespace Algorithm_Implementation
             static readonly string decompressedFile = "C:/Users/Rehman Ali/Desktop/abcd.txt";
             static void Main(string[] args)
             {
-
+            CompressDocxFile();
             CompressTextFile();
             CompressPdfFile();
-            
-            //string wordString = GetTextFromWord();
-            //Console.WriteLine(wordString);
-            //Console.ReadLine();
-        }
+            }
 
         static public void CompressTextFile()
         {
@@ -75,7 +73,7 @@ namespace Algorithm_Implementation
    
             HuffmanTree huffmanTree = new HuffmanTree();
 
-            Console.WriteLine("Pdf File read Successfully\n");
+            Console.WriteLine("Pdf File Read Successfully\n");
 
             // Build the Huffman tree
             huffmanTree.Build_Tree(pdfString);
@@ -90,7 +88,7 @@ namespace Algorithm_Implementation
             // write all the bits from byte array in bin file
             File.WriteAllBytes(BinaryFile, bytes);
             
-            Console.Write("Pdf File Encoded successfully\n");
+            Console.Write("Pdf File Encoded Successfully\n");
 
             // Decode the bin file and write in pdf file
             // read all the bytes from binary file
@@ -125,110 +123,64 @@ namespace Algorithm_Implementation
             return text.ToString();
         }
 
-        static public string GetTextFromWord()
+        static public string GetTextFromWordFile()
         {
-            StringBuilder text = new StringBuilder();
-            Application word = new Application();
-            object miss = System.Reflection.Missing.Value;
-            object path = docxFile;
-            object readOnly = true;
-            Microsoft.Office.Interop.Word.Document docs = word.Documents.Open(ref path, ref miss, ref readOnly, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss);
+            ComponentInfo.SetLicense("AKSJUY-9IUEY-2YUW7-HSGDT-6NHJY");
 
-            for (int i = 0; i < docs.Paragraphs.Count; i++)
-            {
-                text.Append(" \r\n " + docs.Paragraphs[i + 1].Range.Text.ToString());
-            }
-            return text.ToString();
+            // Load Word document from file's path.
+            var document = DocumentModel.Load(docxFile);
+
+            // Get Word document's plain text.
+            string text = document.Content.ToString();
+            return text;
         }
 
-        // for word file compression
-        static void wordfile()
+
+        static public void CompressDocxFile()
         {
-            /*Application application = new Application();
-            Document document = application.Documents.Open(docxFile);
-
-            // Loop through all words in the document.
-            int count = document.Words.Count;
-            for (int i = 1; i <= count; i++)
-            {
-                // Write the word.
-                string text = document.Words[i].Text;
-                
-            }
-            Console.WriteLine(document);
-            // Close word.
-            application.Quit();
-
-
-            /*
-            DocX doc = DocX.Load(docxFile);
-            string text = DocX.Create(docxFile);
-            TextExtractor extractor = new TextExtractor(docxFile);
-            string text = extractor.ExtractText(); //The string 'text' is now loaded with the text from the Word Document
-            Console.WriteLine(text);
-
+            string docxString = GetTextFromWordFile();
 
             HuffmanTree huffmanTree = new HuffmanTree();
 
-            /* Console.WriteLine("File read Successfully\n");
+            Console.WriteLine("Docx File Read Successfully\n");
 
-             // Build the Huffman tree
-             huffmanTree.Build_Tree(file1);
+            // Build the Huffman tree
+            huffmanTree.Build_Tree(docxString);
 
-         // Encode the input file in BitArray in binary form
-         BitArray bit_array = huffmanTree.Encode(inputFile);
+            // Encode the input file in BitArray in binary form
+            BitArray bit_array = huffmanTree.Encode(docxString);
 
-         // Byte array for storing the bits from BitArray to save in bin file
-         byte[] bytes = new byte[bit_array.Length / 8 + (bit_array.Length % 8 == 0 ? 0 : 1)];
-         bit_array.CopyTo(bytes, 0);
+            // Byte array for storing the bits from BitArray to save in bin file
+            byte[] bytes = new byte[bit_array.Length / 8 + (bit_array.Length % 8 == 0 ? 0 : 1)];
+            bit_array.CopyTo(bytes, 0);
 
-         // write all the bits from byte array in bin file
-         File.WriteAllBytes(BinaryFile, bytes);
-         Console.Write("File encoded successfully\n");
-         Console.WriteLine();
+            // write all the bits from byte array in bin file
+            File.WriteAllBytes(BinaryFile, bytes);
 
-         // Decode the bin file and write in txt file
-         // read all the bytes from binary file
-         byte[] bytes2 = File.ReadAllBytes(BinaryFile);
-         var bitarray = new BitArray(bytes2);
+            Console.Write("Docx File Encoded Successfully\n");
 
-         // decode the huffman tree
-         string decoded = huffmanTree.Decode(bitarray);
+            // Decode the bin file and write in docx file
+            // read all the bytes from binary file
+            byte[] bytes2 = File.ReadAllBytes(BinaryFile);
+            var bitarray = new BitArray(bytes2);
 
-         // write the decoded file in txt file
-         File.WriteAllText(decompressedFile, decoded);
-         Console.WriteLine("File Decoded Successfuly\n");
-         Console.ReadLine();
+            // decode the huffman tree
+            string decoded = huffmanTree.Decode(bitarray);
 
+            // write the decoded file in docx file
+            // Create new empty document.
+            var document = new DocumentModel();
 
+            // Add new section with two paragraphs, containing some text and symbols.
+            document.Sections.Add(
+                new GemBox.Document.Section(document,
+                    new GemBox.Document.Paragraph(document,decoded)));
 
-         Application word = new Application();
+            // Save Word document to file's path.
+            document.Save("C:/Users/Rehman Ali/Desktop/abcd.docx");
+            Console.WriteLine("Docx File Decoded Successfuly\n");
 
-         Microsoft.Office.Interop.Word.ApplicationClass wordApp = new ApplicationClass();
-
-         object file = "C:/Users/Rehman Ali/Desktop/abc.docx";
-
-         object missed = System.Reflection.Missing.Value;
-
-         Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(ref file, ref missed, ref missed,
-
-                                               ref missed, ref missed, ref missed,
-
-                                               ref missed, ref missed, ref missed,
-
-                                               ref missed, ref missed, ref missed);
-
-         doc.ActiveWindow.Selection.WholeStory();
-
-         doc.ActiveWindow.Selection.Copy();
-
-         object Clipboard = null;
-         IDataObject data = Clipboard.GetDataObject();
-         object txtFileContent = null;
-         txtFileContent.Text = data.GetData(DataFormats.Text).ToString();
-
-         doc.Close();*/
         }
-
-    }
-    }
+        
+}
+}
